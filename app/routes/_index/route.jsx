90358570372d68ea82206f57be6fca79f3c7f8fa
javediqbl8381 <javediqbal8381@gmail.com>
2026-01-23 22:18:05 +0500

    initial

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -18,7 +18,7 @@ export default function App() {
   return (
     <div className={styles.index}>
       <div className={styles.content}>
-        <h1 className={styles.heading}>A short heading about checkou++</h1>
+        <h1 className={styles.heading}>A short heading about checkou++ {process.env.DATABASE_URL}</h1>
         <p className={styles.text}>
           A tagline about [your app] that describes your value proposition.
         </p>
